de462daad19ea12e354323097c567fd39b4aaf5f
wanchalong <nueng35@gmail.com>
2016-04-24 14:11:05 +0700

    mm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-
+var i
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
@@ -10,6 +10,19 @@ app.get('/webhook/', function (req, res) {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong validation token')
+})
+
+app.post('/webhook/', function (req, res) {
+  var messaging_events = req.body.entry[0].messaging
+  for (var i = 0; i < messaging_events.length; i++) {
+    var event = req.body.entry[0].messaging[i]
+    var sender = event.sender.id
+    if (event.message && event.message.text) {
+      var text = event.message.text
+      console.log(text)
+    }
+  }
+  res.sendStatus(200)
 })
 
 app.set('port', (process.env.PORT || 5000))
