3c8fe5745de3ab44af3a3b5a159b1b246792b6bc
wanchalong <nueng35@gmail.com>
2016-05-29 10:00:02 +0700

    test3
    test3

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/webhook/', function (req, res) {
         sendTextMessage(sender, 'รักเหมือนกัน จุ๊บจุ๊บ <3')
       }
       else if (text.substring(0, 3) === 'sum') {
-        sendTextMessage(sender, text.substring(4, 6) + text.substring(7, 9))
+        sendTextMessage(sender, text.substring(4, 6) + text.substring(6, 8))
       }
     // sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
     }
